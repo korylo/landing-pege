e1272b991f90c3d1488cdbf13ab3147ccb6dd09e
Korylo <korylo@email.com>
2025-05-27 23:08:49 -0300

    atualizando as imagens das demais seções

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,19 +9,14 @@ const contaAsHoras = setInterval(function () {
 
     const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
 
-    const diaEmMs = 1000 * 60 * 60 * 24;
+    const diaEmMs = 1000 * 60 * 60 * 26;
     const horaEmMs = 1000 * 60 * 60;
     const minutoEmMs = 1000 * 60;
 
     const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
-    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horasEmMs);
-    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutoEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
     const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
-
-    console.log(diasAteOEvento);
-    console.log(horasAteOEvento);
-    console.log(minutosAteOEvento);
-    console.log(segundosAteOEvento);
 
     document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
 
